9d8e790796d0a0582666c39bc97cbd1c55e71c35
Pablo Mendez <mendez.developer@gmail.com>
2019-02-24 01:30:22 -0300

    added private route

---
+++ src/components/auth/AuthRoute.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import decode from 'jwt-decode'
+
+const checkAuth = () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return false;
+  }
+  
+  try {
+    const { exp } = decode(token);
+
+    if (exp < new Date().getTime() / 1000) {
+      return false
+    }
+  } catch (e) {
+    return false;
+  }
+
+  return true
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    checkAuth() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{ pathname: '/signin' }} />
+    )
+  )} />
+)
+
+export default AuthRoute
